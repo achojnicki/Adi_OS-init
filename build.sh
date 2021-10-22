87f058cbf91837d920b5145bf60c245512ce885f
achojnicki <adrian@chojnicki.info>
2021-10-22 15:28:52 +0100

    --

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 
 export LDFLAGS="-static"
 
-gcc -c main.c $(python3-config --includes)
+gcc -c main.c $(python3-config --includes) -static
 gcc -o adios-init main.o $(python3-config --ldflags) -static
