dcc50a7f6a49e621ea732565867898fffe3fefcf
achojnicki <adrian@chojnicki.info>
2021-10-23 18:26:48 +0100

    --

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-export LDFLAGS="-static"
+#export LDFLAGS="-static"
 
-gcc -c main.c $(python3-config --includes)
+#gcc -c main.c $(python3-config --includes) -static -static-libgcc -static-libstdc++
 
-gcc -o adios-init main.o -static -static-libgcc -static-libstdc++
+#gcc -o adios-init main.o $(python3-config --ldflags) -static -static-libgcc -static-libstdc++
+#gcc -o adios-init main.o -static -static-libgcc -static-libstdc++
+gcc -static main.c -o adios-init $(pkg-config --libs --cflags python3) -lm -lutil -ldl -lz -lexpat -lpthread -lc
