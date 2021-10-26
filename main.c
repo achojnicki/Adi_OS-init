c6f0845c164f20532b5ed02fc9509619b0e0ef64
Adrian Chojnicki <adrian@chojnicki.info>
2021-10-26 19:00:45 +0000

    --

--- main.c
+++ main.c
@@ -1,21 +1,12 @@
-#define PY_SSIZE_T_CLEAN
-#include <Python.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int
-main(int argc, char *argv[])
-{
-    wchar_t *program = Py_DecodeLocale(argv[0], NULL);
-    if (program == NULL) {
-        fprintf(stderr, "Fatal error: cannot decode argv[0]\n");
-        exit(1);
-    }
-    Py_SetProgramName(program);  /* optional but recommended */
-    Py_Initialize();
-    PyRun_SimpleString("from time import time,ctime\n"
-                       "print('Today is', ctime(time()))\n");
-    if (Py_FinalizeEx() < 0) {
-        exit(120);
-    }
-    PyMem_RawFree(program);
-    return 0;
-}
+int main () {
+   char command[50];
+
+   strcpy(command, "/bin/python /Adi_OS/init.py" );
+   system(command);
+
+   return(0);
+} 
